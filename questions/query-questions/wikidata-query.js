48cdda419028c4c1b58c93ac844b1ede304c8428
Laura Cordero <uo275955@uniovi.es>
2024-03-05 19:11:04 +0100

    Wikidata devuleve objects

--- questions/query-questions/wikidata-query.js
+++ questions/query-questions/wikidata-query.js
@@ -1,10 +1,23 @@
-const WBK = require('wikibase-sdk')
-const wdk = WBK({
-  instance: 'https://www.wikidata.org',
-  sparqlEndpoint: 'https://query.wikidata.org/sparql'
-})
-const sparql = 'SELECT * WHERE { ?s ?p ?o } LIMIT 10'
-const url = wdk.sparqlQuery(sparql)
-result = request({ method: 'GET', url})
+class SPARQLQueryDispatcher {
+	constructor( endpoint ) {
+		this.endpoint = endpoint;
+	}
 
-print(result);
+	query( sparqlQuery ) {
+		const fullUrl = this.endpoint + '?query=' + encodeURIComponent( sparqlQuery );
+		const headers = { 'Accept': 'application/sparql-results+json' };
+
+		return fetch( fullUrl, { headers } ).then( body => body.json() );
+	}
+}
+
+const endpointUrl = 'https://query.wikidata.org/sparql';
+const sparqlQuery = `
+SELECT ?directorLabel WHERE {
+  ?director wdt:P31 wd:Q2526255;
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es". }
+} LIMIT 100
+`;
+
+const queryDispatcher = new SPARQLQueryDispatcher( endpointUrl );
+queryDispatcher.query( sparqlQuery ).then( console.log );
