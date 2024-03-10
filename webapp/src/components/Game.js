5a5512a21b900b75a5d82c99bcceab09f4afa3c6
Pablo López Tamargo <uo287694@uniovi.es>
2024-03-10 11:15:20 +0100

    Grid layout en pantalla game

--- webapp/src/components/Game.js
+++ webapp/src/components/Game.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Container, styled } from '@mui/system';
+import Grid from '@mui/material/Grid';
 
 const StyledContainer = styled(Container)({
     textAlign: 'center',
@@ -18,10 +19,20 @@ const StyledContainer = styled(Container)({
     return (
       <StyledContainer>
         <h1>Pregunta</h1>
+        <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
         <StyledButton>1</StyledButton>
+        </Grid>
+        <Grid item xs={12} sm={6}>
         <StyledButton>2</StyledButton>
+        </Grid>
+        <Grid item xs={12} sm={6}>
         <StyledButton>3</StyledButton>
+        </Grid>
+        <Grid item xs={12} sm={6}>
         <StyledButton>4</StyledButton>
+        </Grid>
+        </Grid>
       </StyledContainer>
     );
   };
