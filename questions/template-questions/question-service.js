c5a3bb7528c3c142617a6fe03e06515c3a596eb8
uo283182 <uo283182@uniovi.es>
2024-03-27 00:35:11 +0100

    Más preguntas

--- questions/template-questions/question-service.js
+++ questions/template-questions/question-service.js
@@ -5,52 +5,72 @@ const Wikidata = require('./wikidata-query');
 const app = express();
 const port = 8004;
 
-const jsonPreg=[
+const jsonPreg = [
   {
-    textStart:'¿Cuál es la capital de ',
-    textEnd:'?',
-    queryCorrect:'SELECT ?preguntaLabel ?respuestaLabel WHERE {' + 
+    textStart: '¿Cuál es la capital de ',
+    textEnd: '?',
+    queryCorrect: 'SELECT ?preguntaLabel ?respuestaLabel WHERE {' +
       '?pregunta wdt:P31 wd:Q6256. ?pregunta wdt:P36 ?respuesta. SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es".}}'
   },
   {
-    textStart:'¿Quién es el director de la película ',
-    textEnd:'?',
-    queryCorrect:'SELECT DISTINCT ?preguntaLabel ?respuestaLabel WHERE {'+
-      '?pregunta wdt:P31 wd:Q11424.'+
-      '?pregunta wdt:P57 ?respuesta.'+
-      'SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es". }'+
-    '} LIMIT 100'
+    textStart: '¿Quién es el director de la película ',
+    textEnd: '?',
+    queryCorrect: 'SELECT DISTINCT ?preguntaLabel ?respuestaLabel WHERE {' +
+      '?pregunta wdt:P31 wd:Q11424.' +
+      '?pregunta wdt:P57 ?respuesta.' +
+      'SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es". }' +
+      '} LIMIT 100'
   },
   {
-    textStart:'¿Quién es el autor del libro ',
-    textEnd:'?',
-    queryCorrect:'SELECT ?preguntaLabel ?respuestaLabel WHERE {'+
-      '?pregunta wdt:P31 wd:Q7725634.'+
-      '?pregunta wdt:P50 ?respuesta.'+
-      'SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es". }'+
-    '} LIMIT 100'
+    textStart: '¿Quién es el autor del libro ',
+    textEnd: '?',
+    queryCorrect: 'SELECT ?preguntaLabel ?respuestaLabel WHERE {' +
+      '?pregunta wdt:P31 wd:Q7725634.' +
+      '?pregunta wdt:P50 ?respuesta.' +
+      'SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es". }' +
+      '} LIMIT 100'
   },
   {
-    textStart:'¿Quién es el cantante de ',
-    textEnd:'?',
-    queryCorrect:'SELECT ?preguntaLabel ?respuestaLabel WHERE {'+
-      '?pregunta wdt:P31 wd:Q134556.'+
-      '?pregunta wdt:P175 ?respuesta.'+
-      'SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es". }'+
-    '} LIMIT 100'
+    textStart: '¿Quién es el cantante de ',
+    textEnd: '?',
+    queryCorrect: 'SELECT ?preguntaLabel ?respuestaLabel WHERE {' +
+      '?pregunta wdt:P31 wd:Q134556.' +
+      '?pregunta wdt:P175 ?respuesta.' +
+      'SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es". }' +
+      '} LIMIT 100'
   },
   {
-    textStart:'¿Dónde se encuentra el monumento ',
-    textEnd:'?',
-    queryCorrect:'SELECT ?preguntaLabel ?respuestaLabel WHERE {'+
-      '?pregunta wdt:P31 wd:Q570116.'+
-      '?pregunta wdt:P17 ?respuesta.'+
-      'SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es". }'+
-    '} LIMIT 100'
+    textStart: '¿Dónde se encuentra el monumento ',
+    textEnd: '?',
+    queryCorrect: 'SELECT ?preguntaLabel ?respuestaLabel WHERE {' +
+      '?pregunta wdt:P31 wd:Q570116.' +
+      '?pregunta wdt:P17 ?respuesta.' +
+      'SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es". }' +
+      '} LIMIT 100'
+  },
+  {
+    textStart: '¿Cuál es la población de ',
+    textEnd: '?',
+    queryCorrect: 'SELECT DISTINCT ?preguntaLabel ' +
+      '(CONCAT(REPLACE(STR((ROUND(?respuesta / 1000) * 1000)), "(\\\\d)(?=(\\\\d{3})+$)", "$1."), "") AS ?respuestaLabel) WHERE {' +
+      '?pregunta wdt:P31 wd:Q6256.' +
+      '?pregunta wdt:P1082 ?respuesta.' +
+      'SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es". }}'
+  }
+  ,
+  {
+    textStart: '¿Cuál es el gentilicio de ',
+    textEnd: '?',
+    queryCorrect: 'SELECT DISTINCT ?preguntaLabel ?respuestaLabel WHERE {'+
+      '?pregunta wdt:P31 wd:Q2074737; '+
+                 'wdt:P17 wd:Q29; '+
+                 'wdt:P1549 ?respuesta.'+
+      'OPTIONAL { ?respuesta rdfs:label ?respuestaLabel. FILTER(LANG(?respuestaLabel) = "es") }'+
+      'SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es". }}'
   }
 
 ]
-;
+  ;
 
 //const json = JSON.parse(jsonPreg);
 
@@ -67,7 +87,7 @@ app.post("/questions", async (req, res) => {
     //Escoge un valor aleatorio para escoger la pregunta
     let sizeJson = jsonPreg.length;
     let randQuery= Math.floor(Math.random() * sizeJson);
-
+    
     wiki.ejecutarConsultaSPARQL(jsonPreg[randQuery].queryCorrect)
       .then((resultados) => {
         //Escoge un valor aleatorio de la consulta para la respuesta correcta
@@ -78,7 +98,7 @@ app.post("/questions", async (req, res) => {
         let randoms = [];
         while (randoms.length < 3) {
           let numero = Math.floor(Math.random() * size);
-          if (!randoms.includes(numero) && numero!=random) {
+          if (!randoms.includes(numero) && numero != random) {
             randoms.push(numero);
           }
         }
@@ -92,18 +112,18 @@ app.post("/questions", async (req, res) => {
             resultados.results.bindings[randoms[2]].respuestaLabel.value
           ]
         }
-            
+
         // console.log(resultadosGuardados.incorrectas);
         // console.log(resultados.results.bindings[randoms[0]].capitalLabel.value);
         // console.log(resultados.results.bindings[randoms[1]].capitalLabel.value);
         // console.log(resultados.results.bindings[randoms[2]].capitalLabel.value);
-          
+
         res.send(resultadosGuardados);
       })
       .catch((error) => {
         console.error('Error al ejecutar la consulta:', error);
       });
-    
+
   } catch (error) { //Catch del try
     console.error("Error:", error); // Maneja el error
     res.status(500).send(error); // Envía una respuesta de error al cliente
@@ -111,13 +131,13 @@ app.post("/questions", async (req, res) => {
 });
 
 const server = app.listen(port, () => {
-    console.log(`User Service listening at http://localhost:${port}`);
-  });
-  
-  // Listen for the 'close' event on the Express.js server
-  server.on('close', () => {
-      // Close the Mongoose connection
-      //mongoose.connection.close();
-    });
-  
-    module.exports = server
+  console.log(`User Service listening at http://localhost:${port}`);
+});
+
+// Listen for the 'close' event on the Express.js server
+server.on('close', () => {
+  // Close the Mongoose connection
+  //mongoose.connection.close();
+});
+
+module.exports = server
