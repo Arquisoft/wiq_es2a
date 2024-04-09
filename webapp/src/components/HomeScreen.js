b6dc151ad2f92b79c40cb71d7d5cd2ae9fa590f3
uo283182 <uo283182@uniovi.es>
2024-04-09 23:53:09 +0200

    Añadido slider para elegir num preguntas

--- webapp/src/components/HomeScreen.js
+++ webapp/src/components/HomeScreen.js
@@ -1,22 +1,16 @@
 import React, { useState,useEffect } from 'react';
-import { Container} from '@mui/material';
-import Game from './Game';
+import { Container,Slider,TextField} from '@mui/material';
 import './HomeScreen.css';
 import { useNavigate } from "react-router-dom";
 
 const HomeScreen = () => {
-    const [defecto, setDefecto] = useState("15");
-
+    const [value, setValue] = useState(15);
     const navigate = useNavigate();
     const token = localStorage.getItem('token');
 
     const handleStartButtonClick = () => {
-      localStorage.setItem('numQuestions', defecto);
+      localStorage.setItem('numQuestions', value);
       navigate("/game");
-    };
-
-    const changeNumber = (event) => {
-      setDefecto(event.target.value);
     };
 
     const handleHistoryButton = () => {
@@ -31,15 +25,44 @@ const HomeScreen = () => {
   useEffect(() => {
     checkUserLogin();
   }, [])
+  
 
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
     return (
         <Container component="main">
         <div>
             <div id = "home">
-              <div className="form-outline" >
+              <div className="form-outline" id="formNumPreguntas" >
                 <label className="form-label" htmlFor="typeNumber">Número de preguntas:</label>
-                <input min="1" max="30" value={defecto} type="number" id="typeNumber" className="form-control" onChange={changeNumber}/>
-              </div>
+                
+        <TextField
+        id="outlined-basic"
+        variant="outlined"
+        value={value}
+        color="info"
+
+        inputProps={{
+          min: 1,
+          max: 30,
+          type: 'number',
+          'aria-labelledby': 'input-slider',
+        }}
+        onChange={(e) => {
+          const newValue = e.target.value === '' ? 0 : Number(e.target.value);
+          setValue(Math.min(Math.max(newValue, 1), 30));
+        }}
+      />
+        <Slider
+        value={value}
+        onChange={handleChange}
+        min={1}
+        max={30}
+        color="info"
+      />
+      
+        </div>
               <button onClick={handleStartButtonClick} type="button" className="btn btn-outline-primary btn-lg">Jugar</button>
               <button  onClick={handleHistoryButton} type="button" className="btn btn-outline-primary btn-lg">Ver historial</button>
             </div>
