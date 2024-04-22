041f34d7342a88ba48b760afd6a8ae2cd8ec1726
uo283182 <uo283182@uniovi.es>
2024-04-22 13:43:19 +0200

    test de App.js

--- webapp/src/App.test.js
+++ webapp/src/App.test.js
@@ -1,11 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen,waitFor } from '@testing-library/react';
 import { BrowserRouter} from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>
-  );
+describe('App component', () => {
+
+  it('renders learn react link',async () => {
+    render(
+      <BrowserRouter>
+          <App/>
+      </BrowserRouter>
+    );
+    
+      // Esperamos que se muestre el boton de cerrar sesión
+      await waitFor(() => {
+        const navElement = screen.getByRole('navigation');
+        expect(navElement).toBeInTheDocument();
+        expect(screen.getByText(/Cerrar sesión/i)).toBeInTheDocument();
+      });
+  });
+
 });
