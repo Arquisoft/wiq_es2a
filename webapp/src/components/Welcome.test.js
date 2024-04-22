6186be34e1b2c9ccb766322380d8d6917e6262c3
uo283182 <uo283182@uniovi.es>
2024-04-22 21:17:58 +0200

    Tests de Welcome.js

---
+++ webapp/src/components/Welcome.test.js
@@ -0,0 +1,42 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { BrowserRouter} from 'react-router-dom';
+import Welcome from './Welcome';
+
+describe('Welcome Component', () => {
+  
+  it('should render the welcome message', () => {
+    render( 
+        <BrowserRouter>
+          <Welcome />
+        </BrowserRouter>
+    );
+    const welcomeMessage = screen.getByText(/Tu juego favorito de televisión, ¡ahora en tu ordenador!/i);
+    expect(welcomeMessage).toBeInTheDocument();
+    const welcomeMessage2 = screen.getByText(/Accede a tu cuenta o regístrate para comenzar/i);
+    expect(welcomeMessage2).toBeInTheDocument();
+    
+  });
+
+  it('should navigate to login page when login button is clicked', () => {
+    render( 
+        <BrowserRouter>
+          <Welcome />
+        </BrowserRouter>
+    );
+    const loginButton = screen.getByRole('button', { name: /login/i });
+    fireEvent.click(loginButton);
+    expect(window.location.pathname).toBe('/login');
+  });
+
+  it('should navigate to signup page when signup button is clicked', () => {
+    render( 
+        <BrowserRouter>
+          <Welcome />
+        </BrowserRouter>
+    );
+    const signupButton = screen.getByRole('button', { name: /signup/i });
+    fireEvent.click(signupButton);
+    expect(window.location.pathname).toBe('/adduser');
+  });
+
+});
