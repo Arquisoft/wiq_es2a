e7a3232941fa0aca739c9954d8cdbab9cb299384
Laura Cordero <uo275955@uniovi.es>
2024-04-25 18:17:57 +0200

    Completing user-service test

--- users/userservice/user-service.test.js
+++ users/userservice/user-service.test.js
@@ -27,4 +27,43 @@ describe('User Service', () => {
     expect(response.status).toBe(200);
     expect(response.body).toHaveProperty('username', 'testuser');
   });
+
+  it('should return 400 if username is empty or blank', async () => {
+    const userError1 = {
+        username: ' ',
+        password: 'testpassword',
+    };
+
+    const response = await request(app).post('/adduser').send(userError1);
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty('error', 'El nombre de usuario no puede estar vacío');
+  });
+
+  it('should return 400 if password is empty or blank', async () => {
+    const userError2 = {
+        username: 'testusername',
+        password: ' ',
+    };
+
+    const response = await request(app).post('/adduser').send(userError2);
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty('error', 'La contraseña no puede estar vacía');
+  });
+
+  it('should return 400 if there is it misses information', async () => {
+    const userError4 = {
+      username: 'wrongFormat',
+    };
+
+    const response = await request(app).post('/adduser').send(userError4);
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty('error', 'Missing required field: password');
+  });
+
+  //For getting all users
+  it('should GET all the users', async () => {
+    const response = await request(app).get('/getAllUsers');
+    expect(response.statusCode).toBe(200);
+    expect(Array.isArray(response.body)).toBeTruthy();
+  });
 });
