f545b7d786f17fb6945cd79172588f78107fa17a
Laura Cordero <uo275955@uniovi.es>
2024-04-26 13:12:13 +0200

    Another game test

--- webapp/src/components/Game.test.js
+++ webapp/src/components/Game.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen,cleanup } from '@testing-library/react';
+import { render, screen,cleanup, waitFor, fireEvent } from '@testing-library/react';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import Game from './Game';
@@ -29,10 +29,11 @@ describe('Game', () => {
 
   it('should mock the question and show it on the screen', async () => {
     const mockResponse = {
-      pregunta: 'What is the capital of France?',
-      correcta: 'Paris',
-      incorrectas: ['London', 'Berlin', 'Madrid'],
+      pregunta: '¿Cual es la capital de Italia?',
+      correcta: 'Roma',
+      incorrectas: ['Seul', 'Berlin', 'Madrid'],
     };
+
     mockAxios.onPost('http://localhost:8000/questions').reply(200, mockResponse);
   
     render(
@@ -40,10 +41,37 @@ describe('Game', () => {
         <Game />
     </BrowserRouter>);
 
-    await screen.findByText('What is the capital of France?');
-    expect(screen.getByText('Paris')).toBeInTheDocument();
-    expect(screen.getByText('London')).toBeInTheDocument();
+    await screen.findByText('¿Cual es la capital de Italia?');
+    expect(screen.getByText('Roma')).toBeInTheDocument();
+    expect(screen.getByText('Seul')).toBeInTheDocument();
     expect(screen.getByText('Berlin')).toBeInTheDocument();
     expect(screen.getByText('Madrid')).toBeInTheDocument();
   });
+
+  it('should select the correct answer and add 1 to the total', async () => {
+
+    const mockResponse = {
+      pregunta: '¿Cual es la capital de Italia?',
+      correcta: 'Roma',
+      incorrectas: ['Seul', 'Berlin', 'Madrid'],
+    };
+
+    mockAxios.onPost(`http://localhost:8000/questions`).reply(200, mockResponse);
+
+    const { getByText } = render(
+      <BrowserRouter>
+          <Game />
+      </BrowserRouter>);
+
+    // Esperar a que se cargue la pregunta
+    await waitFor(() => expect(getByText(mockResponse.pregunta)).toBeInTheDocument());
+
+    // Seleccionamos la respuesta correcta
+    const correctOption = screen.getAllByRole('radio').find(option => option.parentElement.getAttribute('data-iscorrect') === 'true');
+    fireEvent.click(correctOption);
+
+    // Verificar que se incrementa la puntuación
+    expect(getByText('Preguntas acertadas: 1')).toBeInTheDocument();
+  });
+
 });
